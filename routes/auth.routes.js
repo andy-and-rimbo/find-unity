a65c8007a8731dc54f4d0b7097846b38febdde34
Andy <andrew.mcginn@gmail.com>
2021-04-28 17:58:34 +0100

    capitalise helper added for names on profiles

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -26,7 +26,9 @@ router.post("/signup", (req, res, next) => {
   //! uncomment this after development
   // if(!(password.match(/\W/g) && password.match(/\d/g) && password.match(/\w|_/g)) || password.length < 10) 
   //   return res.render("auth/signup", {message: "Please make sure your password includes at least one letter, number and special character. It must also be at least 10 characters long."});
-
+    username.replace(/^\w/, (c) => c.toUpperCase());
+    console.log('capitalised username', username);
+    
 
     User.findOne({username:username})
     .then(user => {
